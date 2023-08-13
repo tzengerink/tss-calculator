1669ab5c0abe3c983508d547671e7c228cd89ea7
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2023-08-13 14:59:06 +0200

    Improve responsive design

--- src/components/TssCalculator.vue
+++ src/components/TssCalculator.vue
@@ -3,6 +3,8 @@ import { useCalculator } from '../composables/useCalculator'
 import { Input } from '../types/calculator'
 import TssCalculatorInput from './TssCalculatorInput/TssCalculatorInput.vue'
 import TssCalculatorOutput from './TssCalculatorOutput.vue'
+import CalculatorIcon from './CalculatorIcon.vue'
+import ResetIcon from './ResetIcon.vue'
 
 const { input, output, reset } = useCalculator()
 
@@ -12,14 +14,18 @@ const onChange = (newInput: Input) => {
 </script>
 
 <template>
-  <div class="bg-cpwhite">
-    <div class="flex justify-between p-2">
-      <h1 class="font-normal text-3xl text-cppurple">TSS CALCULATOR</h1>
+  <div class="bg-cpwhite max-w-md mx-auto mt-2 md:mt-8 md:shadow-xl">
+    <div class="flex justify-between p-4 border-b border-b-cpgray">
+      <h1 class="flex flex-row font-normal text-2xl text-cppurple">
+        <CalculatorIcon class="mt-1 mr-2" />
+        <span>TSS Calculator</span>
+      </h1>
       <button
-        class="px-2 text-cpgray border border-cpgray rounded hover:text-cppink hover:border-cppink"
+        class="px-2 text-2xl text-cpgray border border-cpgray rounded hover:text-cppink hover:border-cppink"
+        title="Reset"
         @click="reset"
       >
-        Reset
+        <ResetIcon />
       </button>
     </div>
     <TssCalculatorInput :input="input" @change="onChange" />
