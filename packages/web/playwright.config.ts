75e799285eba8e7328d7e8c73a0935b4245df859
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2023-08-22 12:50:32 +0200

    Use workspaces for code organisation

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'list',
   use: {
